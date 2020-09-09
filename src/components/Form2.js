80b36a87bee665aa3b4a5141290568286f028faa
AlexVcom <doalvor@gmail.com>
2020-09-09 21:10:46 +0200

    form err

---
+++ src/components/Form2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+
+class Form2 extends Component {
+
+    render() {
+        return <div>
+                
+            </div>
+    }
+}
+
+export default Form2;
